b9fa4e0dc06fb6584d14b9021724fa8874970e31
Ben4three <ninjaben2222@gmail.com>
2026-06-10 07:24:13 -0500

    Create players.ts

---
+++ components/components/data/players.ts
@@ -0,0 +1,31 @@
+export const players = [
+  {
+    id: 1,
+    name: "LeBron James",
+    overall: 97,
+    position: "SF",
+    image: "/images/lebron.png",
+
+    attributes: {
+      threePoint: 88,
+      drivingDunk: 95,
+      speed: 89,
+      ballHandle: 86,
+    },
+  },
+
+  {
+    id: 2,
+    name: "Stephen Curry",
+    overall: 96,
+    position: "PG",
+    image: "/images/curry.png",
+
+    attributes: {
+      threePoint: 99,
+      drivingDunk: 60,
+      speed: 91,
+      ballHandle: 97,
+    },
+  },
+];
